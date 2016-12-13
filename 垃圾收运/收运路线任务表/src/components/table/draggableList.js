272273692eda2d0de9dccaeb440b1e164c31d7c9
TianQi Zhang <jsxsztq@163.com>
2016-12-13 15:43:32 +0800

    增加：可拖动到数据为空的行内

--- 垃圾收运/收运路线任务表/src/components/table/draggableList.js
+++ 垃圾收运/收运路线任务表/src/components/table/draggableList.js
@@ -30,9 +30,13 @@ const DraggableList = ({rowIndex, dispatch, collectionSpots})=>{
 		dispatch({type:'carTable/moveSpot',payload:{oldSpotIndex,oldRowIndex,newSpotIndex,newRowIndex }});
 		return false;
 	}
-	// const dropOnul = ()=>{
-	// 	dispatch({type:'carTable/moveSpot',payload:{oldSpotIndex,oldRowIndex,newSpotIndex,newRowIndex }});
-	// }
+	const dropOnul = ({e,rowIndex})=>{
+		e.preventDefault();
+		let newRowIndex = rowIndex;
+		let newSpotIndex = 0;
+		let [oldRowIndex, oldSpotIndex] = e.dataTransfer.getData("text").split('|').map((x)=>parseInt(x));
+		dispatch({type:'carTable/moveSpot',payload:{oldSpotIndex,oldRowIndex,newSpotIndex,newRowIndex }});
+	}
 	const dragEnter = ({e,spotIndex,rowIndex})=>{
 		// console.log('in', e.target)
 		dispatch({type:'carTable/moveIn',payload:{spotIndex,rowIndex}});
@@ -42,21 +46,35 @@ const DraggableList = ({rowIndex, dispatch, collectionSpots})=>{
 		dispatch({type:'carTable/moveOut',payload:{spotIndex,rowIndex}});
 	}
 
-	return (
-		<ul>
-		{
-		collectionSpots.map((spot,spotIndex)=>{
-			return (
-				<li className={style.spot_li + " "+ (spot.moveIn ? style.move_in : "")} draggable={"true"} onDragStart={e=>{dragStart({e,spotIndex,rowIndex})}} 
-				onDragOver={e=>dragOver({e,spotIndex,rowIndex})} onDrop={e=>dropOnli({e,spotIndex,rowIndex})} onDragLeave={e=>dragLeave({e,spotIndex,rowIndex})}
-				onDragEnter={e=>dragEnter({e,spotIndex,rowIndex})}>
-					 {spotIndex+1}. {spot.val} 
-					<i className={"iconfont "+style.delSpot}  onClick={()=>{deleteSpot({rowIndex,spotIndex})}}>&#xe67d;</i>
-				</li>
-			)
-		})}
-		</ul>
-	)
+	const liGroup = collectionSpots.map((spot,spotIndex)=>{
+		return (
+			<li className={style.spot_li + " "+ (spot.moveIn ? style.move_in : "")} draggable="true" onDragStart={e=>{dragStart({e,spotIndex,rowIndex})}} 
+			onDragOver={e=>dragOver({e,spotIndex,rowIndex})} onDrop={e=>dropOnli({e,spotIndex,rowIndex})} onDragLeave={e=>dragLeave({e,spotIndex,rowIndex})}
+			onDragEnter={e=>dragEnter({e,spotIndex,rowIndex})}>
+				 {spotIndex+1}. {spot.val} 
+				<i className={"iconfont "+style.delSpot}  onClick={()=>{deleteSpot({rowIndex,spotIndex})}}>&#xe67d;</i>
+			</li>
+		)
+	})
+	// {collectionSpots.length===0 ? onDrop={e=>dropOnul({e,rowIndex})} : ''}
+	// console.log(collectionSpots.length)
+
+	if(collectionSpots.length===0){
+		return (
+			<ul className={style.spotContainer} onDrop={e=>dropOnul({e,rowIndex})} 
+			onDragOver={e=>{e.preventDefault();e.dataTransfer.dropEffect = 'move';}}>
+			{liGroup}
+			</ul>
+		)
+	}
+	else{
+		return (
+			<ul className={style.spotContainer} >
+			{liGroup}
+			</ul>
+		)
+	}
+	
 }
 
 DraggableList.propTypes = {
